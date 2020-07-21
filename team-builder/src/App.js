6dc818a20ea9bfe1129870a209e51af6f2f9d480
Jeremiah Trnka <jeremiah@jeremiahs-mbp.lan>
2020-07-21 13:49:22 -0700

    Updated readme. Updated App.js with revised code to handle state, create API calls, and functions for form update and submit.

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,24 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { v4 as uuid } from 'uuid'
 import './App.css';
 
+const initialTeamList = [
+  {
+    id: uuid(),
+    name: 'Jeremiah',
+    email: 'person@someone.com',
+    role: 'React 1 Developer',
+  },
+]
+
+const initialFormValues = {
+  name: '',
+  email: '',
+  role: '',
+}
+
+const fakeAxiosGet = () => {
+  return Promise.resolve({ status: 200, success: true, data: initialTeamList })
+}
+const fakeAxiosPost = (url, { name, email, role }) => {
+  const newTeamMember = { id: uuid(), name, email, role }
+  return Promise.resolve({ status: 200, success: true, data: newTeamMember })
+}
+
 function App() {
+  const [teamMembers, setTeamMembers] = useState([])
+  const [formValues, setFormValues] = useState(initialFormValues)
+
+  const updateForm = (inputName, inputValue) => {
+    setFormValues({ ...formValues, [inputName]: inputValue })
+  }
+
+  const submitForm = () => {
+    const newTeamMember = {
+      name: formValues.name.trim(),
+    }
+
+    // Prevent form submission if required fields are blank
+    if (!newTeamMember.name) return
+
+    fakeAxiosPost('https://reqres.in/api/users', newTeamMember)
+      .then(res => {
+        const teamMemberFromAPI = res.data
+        setTeamMembers([teamMemberFromAPI, ...teamMembers])
+        setFormValues(initialFormValues)
+      })
+
+      .catch(err => {
+        console.log(`There was an error. The error is ${err}`)
+      })
+  }
+
+  useEffect(() => {
+    fakeAxiosGet('fakeapi.com')
+      .then(res => {
+        setTeamMembers(res.data)
+      })
+
+      .catch(err => {
+        console.log(`There was an error. The error is ${err}`)
+      })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+     
     </div>
   );
 }
